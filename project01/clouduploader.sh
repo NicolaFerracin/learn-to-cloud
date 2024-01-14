ab3fb0d7859a75227293b8e07fc35e7d67a5dbb2
Nicola Ferracin <nicola.ferracin@gmail.com>
2024-01-14 22:15:59 +0000

    Project01 - basic AWS file uploader

--- project01/clouduploader.sh
+++ project01/clouduploader.sh
@@ -15,6 +15,13 @@ setup() {
     provider="gcp";
   else 
     echo "No valid cloud provider specified";
+    exit 1
+  fi
+
+  if [ ! -f "$2" ];
+  then
+    echo "No valid file provided"
+    exit 1
   fi
 }
 
@@ -23,8 +30,9 @@ installAws() {
 
   if [ "$curr_version" = "" ]
   then
-    curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
-    sudo installer -pkg AWSCLIV2.pkg -target /
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
   fi
 
   echo "AWS version: $(aws --version)"
@@ -35,39 +43,69 @@ installAzure() {
 
   if [ "$curr_version" = "" ]
   then
-    brew update && brew install azure-cli
+    curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
   fi
 
   echo "Azure version: $(az --version)"
 }
 
 installGcp() {
-  echo "lol"
+  echo "TBD"
 }
 
-# download the provider CLI if not installed
-install() {
-  if [ "$provider" = "" ];
+loginAws() {
+  hasProfile=$(aws configure get aws_access_key_id --profile default)
+  if [[ "$hasProfile" == "" ]];
   then
-    exit 1;
+    aws configure --profile default
   fi
 
-  if [ "$provider" = "aws" ];
-  then
-    installAws
-  elif [ "$provider" = "azure" ];
-  then
-    installAzure
-  elif [ "$provider" = "gcp" ];
-  then
-    installGcp
-  fi
 }
 
-if (( $# == 0 )); then
+loginAzure() {
+  az login --use-device-code
+}
+
+loginGcp() {
+  echo "TBD"
+}
+
+uploadAws() {
+  read -p "Enter bucket name: " bucketName
+
+  # if bucket already exists this won't do anything
+  aws s3api create-bucket \
+  --bucket "$bucketName" \
+  --region us-east-1 \
+  &> /dev/null
+
+  # actual file upload
+  aws s3 cp "$1" s3://"$bucketName"
+}
+
+if [ $# -eq 0 ];
+then
   echo "No parameters provided"
+  exit 1
+elif [ $# -lt "2" ]
+then
+  echo "Not enough parameters provided"
   exit 1
 fi
 
-setup $1
-install
+setup $1 $2
+
+if [ "$provider" = "aws" ];
+then
+  installAws
+  loginAws
+  uploadAws $2
+elif [ "$provider" = "azure" ];
+then
+  installAzure
+  loginAzure
+elif [ "$provider" = "gcp" ];
+then
+  installGcp
+  loginGcp
+fi
